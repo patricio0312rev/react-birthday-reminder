c52f9d5bf92a410971190725e6734181cd45fd1b
Juan Patricio Marroquin <review0312oficial@gmail.com>
2022-01-17 16:23:45 -0500

    Terminando el componente List

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ function App() {
   return (
     <main>
       <section className='container'>
-        <h3>0 birthdays today</h3>
+        <h3>{people.length} birthdays today</h3>
         <List people={people}/>
 
-        <button onClick={() => console.log('You clicked me')} >Clear all</button>
+        <button onClick={() => setPeople([])} >Clear all</button>
       </section>
     </main>
   );
