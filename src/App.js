fc874022bec9841ecde8e9030d1933993b4ccfbe
Juan Patricio Marroquin <review0312oficial@gmail.com>
2022-01-17 16:20:25 -0500

    Creando hook setPeople y pasando la data como props

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import List from './components/List/list.component';
 import data from './data'
 
 function App() {
+  const [people, setPeople] = useState(data);
+
   return (
     <main>
       <section className='container'>
         <h3>0 birthdays today</h3>
-        <List />
+        <List people={people}/>
 
         <button onClick={() => console.log('You clicked me')} >Clear all</button>
       </section>
